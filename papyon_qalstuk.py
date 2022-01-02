c792ce1ddb5427c640f1d672e616844ebae3929e
RamonParis <87645308+RamonParis@users.noreply.github.com>
2022-01-03 02:17:26 +0400

    Create papyon_qalstuk.py

---
+++ papyon_qalstuk.py
@@ -0,0 +1,16 @@
+#bowtie.py
+# al sene papyon qalstuk
+
+from turtle import *
+pensize (7)
+penup()
+goto(-200, -100)
+pendown()
+fillcolor("red")
+begin_fill()
+goto(-200, 100) 
+goto (200, -100)
+goto (200, 100) 
+goto(-200, -100)
+end_fill()
+exitonclick()
